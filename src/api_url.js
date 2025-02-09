d4b08b5b05bfc8ccfe6f002f287e2c1dc3dce31f
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2025-02-09 19:47:17 +0800

    Update api_url.js

--- src/api_url.js
+++ src/api_url.js
@@ -1,4 +1,4 @@
-const img_url = "https://backendpickmeuptrial-production.up.railway.app"; //<----Change IP address here only
+const img_url = "https://backend-pickmeup.up.railway.app"; //<----Change IP address here only
 // const img_url = "http://192.168.202.124:8000"; //<----Change IP address here only
 const API_URL = img_url + "/api/user/";
 
